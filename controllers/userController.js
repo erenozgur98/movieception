91e0303ff9902225cb26880d24c29a9255362aeb
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-20 13:16:17 -0400

    added folders for components inside src

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,26 @@
-const getUser = (req, res) => {
-    res.send('this works!')
+const db = require('../models');
+
+const getUser = async (req, res) => {
+    try {
+        const users = await db.User.find();
+
+        res.status(200).json(users);
+    } catch(err) {
+        res.status(404).json({ message: err.message })
+    }
 }
 
-module.exports = getUser;
+const createUser = async (req, res) => {
+    const user = req.body;
+
+    const newUser = new db.User(user)
+    try {
+        await newUser.save();
+
+        res.status(201).json(newUser);
+    } catch(err) {
+        res.status(409).json({ message: err.message })
+    }
+}
+
+module.exports = getUser, createUser;
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const getUser = require('../../controllers/userController');
+const createUser = require('../../controllers/userController');
 
 router.use('/', getUser)
+router.post('/create', createUser)
 
 module.exports = router;
