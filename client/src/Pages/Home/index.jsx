79f4db16b6ddfd3795d4e257b4ee326adb42f5e4
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-06 00:43:04 -0400

    adding TODO's inside home page

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+// TODOs:
+// Search bar to search for movies or tv shows
+// Drop down / something similar for the genres so people can sort the listings ( discover, movies, shows )
+// Home will have random show or a movie(s) (3 in a row maybe) underneath the search bar( behind the search bar put the Barney picture )
+
+
+
 function Home() {
     return (
         <div>
