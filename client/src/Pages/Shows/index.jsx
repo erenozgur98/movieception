c0bb066212981948f7448e1656aff6ef071862e6
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-06 00:39:11 -0400

    added and commented out the code inside the shows, for future

--- client/src/Pages/Shows/index.jsx
+++ client/src/Pages/Shows/index.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 function Shows() {
     return (
+        // TODO do the mapping again here too, from discover page
+        // only the genre should be for tv shows only
         <div>
-            
+            {/* <div className="card">
+                <img className="card-img-top" src="..." alt="something" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text"></p>
+                </div>
+                <div className="card-footer">
+                    <small className="text-muted"></small>
+                </div>
+            </div> */}
         </div>
     )
 }
