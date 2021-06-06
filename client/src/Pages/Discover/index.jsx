dcd24d43ab26cb01ea7517665dbce9715affc299
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-06 00:36:02 -0400

    deleted 2 cards that no longer needed, left 1 for the code

--- client/src/Pages/Discover/index.jsx
+++ client/src/Pages/Discover/index.jsx
@@ -14,26 +14,6 @@ function Discover() {
                     <small className="text-muted"></small>
                 </div>
             </div>
-            <div className="card">
-                <img className="card-img-top" src="..." alt="something" />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text"></p>
-                </div>
-                <div className="card-footer">
-                    <small className="text-muted"></small>
-                </div>
-            </div>
-            <div className="card">
-                <img className="card-img-top" src="..." alt="something" />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text"></p>
-                </div>
-                <div className="card-footer">
-                    <small className="text-muted"></small>
-                </div>
-            </div>
         </div>
     )
 }
