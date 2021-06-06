d68eb1c822175b1233b901c9508bf9152a3c71fd
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-06 00:43:58 -0400

    fixed something small

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -4,13 +4,14 @@ import React from 'react'
 // Search bar to search for movies or tv shows
 // Drop down / something similar for the genres so people can sort the listings ( discover, movies, shows )
 // Home will have random show or a movie(s) (3 in a row maybe) underneath the search bar( behind the search bar put the Barney picture )
+// Make a logo
 
 
 
 function Home() {
     return (
         <div>
-            <h1>HI</h1>
+            <h1 className='text-center'>True Story</h1>
         </div>
     )
 }
