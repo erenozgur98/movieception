2ebb3f7e8b7a3a2a0f08f79ae128d37bf0d6c248
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-06 00:38:16 -0400

    added code and commented them out inside the movie, for future

--- client/src/Pages/Movies/index.jsx
+++ client/src/Pages/Movies/index.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 function Movies() {
     return (
+        // TODO do the mapping you are going to do to the Discover page here too
+        // only change the genre for the movies only
         <div>
-            
+            {/* <div className="card">
+                <img className="card-img-top" src="..." alt="something" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text"></p>
+                </div>
+                <div className="card-footer">
+                    <small className="text-muted"></small>
+                </div>
+            </div> */}
         </div>
     )
 }
