0f7ed232945d0cd13fcf7e706a8352bdac0cbc04
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-06 00:35:18 -0400

    added cards to the discover page and comment on what's actually going to happen

--- client/src/Pages/Discover/index.jsx
+++ client/src/Pages/Discover/index.jsx
@@ -2,10 +2,42 @@ import React from 'react'
 
 function Discover() {
     return (
-        <div>
-            
+        // TODO map thru the information to the cards for the discover, after user selects a genre
+        <div className="card-deck">
+            <div className="card">
+                <img className="card-img-top" src="..." alt="something" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text"></p>
+                </div>
+                <div className="card-footer">
+                    <small className="text-muted"></small>
+                </div>
+            </div>
+            <div className="card">
+                <img className="card-img-top" src="..." alt="something" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text"></p>
+                </div>
+                <div className="card-footer">
+                    <small className="text-muted"></small>
+                </div>
+            </div>
+            <div className="card">
+                <img className="card-img-top" src="..." alt="something" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text"></p>
+                </div>
+                <div className="card-footer">
+                    <small className="text-muted"></small>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default Discover
+
+
