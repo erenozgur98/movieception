7ee843958606b2c37724ee2cae9d3e75908d4ec9
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-06 00:36:28 -0400

    commented out the code inside discover for future purposes

--- client/src/Pages/Discover/index.jsx
+++ client/src/Pages/Discover/index.jsx
@@ -4,7 +4,7 @@ function Discover() {
     return (
         // TODO map thru the information to the cards for the discover, after user selects a genre
         <div className="card-deck">
-            <div className="card">
+            {/* <div className="card">
                 <img className="card-img-top" src="..." alt="something" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
@@ -13,7 +13,7 @@ function Discover() {
                 <div className="card-footer">
                     <small className="text-muted"></small>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
