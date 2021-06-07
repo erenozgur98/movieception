627d0ad1daf3efdbcc143d23246078249b8a319a
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-07 18:42:52 -0400

    added more cards inside profile

--- client/src/Pages/Profile/index.jsx
+++ client/src/Pages/Profile/index.jsx
@@ -3,15 +3,40 @@ import { Container } from 'react-bootstrap';
 
 function Profile() {
     return (
-        <div className="card" style="width:400px">
-          <img className="card-img-top" src="img_avatar1.png" alt="Card image" />
-          <div className="card-body">
-            <h4 className="card-title">John Doe</h4>
-            <p className="card-text">Some example text.</p>
-            <a href="/" className="btn btn-primary">See Profile</a>
-          </div>
-        </div>
-        
+        <>
+            <Container>
+                <div className="card">
+                    <img className="card-img-top" src="img_avatar1.png" alt="Card" />
+                    <div className="card-body">
+                        <h4 className="card-title">John Doe</h4>
+                        <p className="card-text">Some example text.</p>
+                        <a href="/" className="btn btn-primary">See Profile</a>
+                    </div>
+                </div>
+                <div className="card-deck">
+                    <div className="card bg-primary">
+                        <div className="card-body text-center">
+                            <p className="card-text">Some text inside the first card</p>
+                        </div>
+                    </div>
+                    <div className="card bg-warning">
+                        <div className="card-body text-center">
+                            <p className="card-text">Some text inside the second card</p>
+                        </div>
+                    </div>
+                    <div className="card bg-success">
+                        <div className="card-body text-center">
+                            <p className="card-text">Some text inside the third card</p>
+                        </div>
+                    </div>
+                    <div className="card bg-danger">
+                        <div className="card-body text-center">
+                            <p className="card-text">Some text inside the fourth card</p>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </>
     )
 }
 
@@ -19,25 +44,3 @@ export default Profile
 
 
 
-{/* <div className="card-deck">
-  <div className="card bg-primary">
-    <div className="card-body text-center">
-      <p className="card-text">Some text inside the first card</p>
-    </div>
-  </div>
-  <div className="card bg-warning">
-    <div className="card-body text-center">
-      <p className="card-text">Some text inside the second card</p>
-    </div>
-  </div>
-  <div className="card bg-success">
-    <div className="card-body text-center">
-      <p className="card-text">Some text inside the third card</p>
-    </div>
-  </div>
-  <div className="card bg-danger">
-    <div className="card-body text-center">
-      <p className="card-text">Some text inside the fourth card</p>
-    </div>
-  </div>
-</div> */}
