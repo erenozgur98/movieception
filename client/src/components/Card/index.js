d05ad5221deda339ac6be7ccb736ed24a2d25466
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-10 23:22:00 -0400

    getting somewhere with the fetching

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Card({ props }) {
+function Card({ image, name, overview }) {
     return (
         <div className="card" style={{ width: "18rem" }}>
-            <img className="card-img-top" src={props.image} alt="Card" />
+            <img className="card-img-top" src={image} alt="Card" />
             <div className="card-body">
-                <h5 className="card-title">{props.name}</h5>
-                <p className="card-text">{props.overview}</p>
+                <h5 className="card-title">{name}</h5>
+                <p className="card-text">{overview}</p>
                 {/* <a href="/" className="btn btn-primary"></a> */}
             </div>
         </div>
