8377a539b5907eb06f4c3a91ad74f1f33c67e90f
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-14 21:09:30 -0400

    commented out the withauth for now

--- client/src/components/WithAuth.jsx
+++ client/src/components/WithAuth.jsx
@@ -1,15 +1,17 @@
 import { Redirect, Route } from 'react-router-dom';
 
 const WithAuth = ({ user, component: Component, ...rest }) => {
-    console.log(user && user.username)
-    if (!user || !user.username) {
-        return <Redirect to="/login" />
-    }
+    // console.log(user && user.username)
+    // if (!user || !user.username) {
+    //     return <Redirect to="/login" />
+    // }
     return (
-        <Route
-            {...rest}
-            render={(props) => <Component {...props} user={user} />}
-        />
+        <>
+        </>
+        // <Route
+        //     {...rest}
+        //     render={(props) => <Component {...props} user={user} />}
+        // />
     )
 }
 
