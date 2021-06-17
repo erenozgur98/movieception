ff5c625f4be20cf450acb7280988aeab57c4d0a0
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-16 21:07:24 -0400

    working progress

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -1,13 +1,17 @@
 import React, { useRef, useState } from 'react'
 import { Container } from 'react-bootstrap'
-import Card from '../../components/Card';
-require('dotenv').config();
+// import Card from '../../components/Card';
+// import { toast } from 'react-toastify';
+
+// toast.configure();
 
 // TODOs:
 // Search bar to search for movies or tv shows
 // Drop down / something similar for the genres so people can sort the listings ( discover, movies, shows )
 // Home will have random show or a movie(s) (3 in a row maybe) underneath the search bar( behind the search bar put the Barney picture )
 // Make a logo
+// Make toast work
+let IMGurl = 'https://image.tmdb.org/t/p/w300';
 
 function Home() {
     const [image, setImage] = useState([]);
@@ -36,24 +40,9 @@ function Home() {
                 console.log(data)
                 if (searchedItem.current.value) {
                     await data.results.forEach(x => {
-                        console.log(result);
-                        console.log(image);
-                        setImage([...image, `${IMGurl}${x.poster_path}`])
-                        setResult([...result])
-                    })
-                }
-            });
-
-        await fetch(tvURL)
-            .then(res => res.json())
-            .then(async res => {
-                console.log(res)
-                if (searchedItem.current.value) {
-                    await res.results.forEach(x => {
-                        console.log(result);
-                        console.log(image);
-                        setImage([...image, `${IMGurl}${x.poster_path}`])
+                        // setImage([...image, { key: x }, `${IMGurl}${x.poster_path}`])
                         setResult([...result, {
+                            image: `${IMGurl}${x.poster_path}`,
                             original_name: x.original_name,
                             overview: x.overview,
                             popularity: x.popularity,
@@ -61,9 +50,42 @@ function Home() {
                             vote_count: x.vote_count,
                             key: x
                         }])
+                        console.log(result);
+                        console.log(image);
                     })
                 }
+                // else {
+                //     toast.warn('You must search for a Show or a Movie!', {
+                //         autoClose: 2500
+                //     });
+                // }
             });
+
+        // await fetch(tvURL)
+        //     .then(res => res.json())
+        //     .then(async res => {
+        //         console.log(res)
+        //         if (searchedItem.current.value) {
+        //             await res.results.forEach(x => {
+        //                 console.log(result);
+        //                 console.log(image);
+        //                 setImage([...image, `${IMGurl}${x.poster_path}`])
+        //                 // setResult([...result, {
+        //                 //     original_name: x.original_name,
+        //                 //     overview: x.overview,
+        //                 //     popularity: x.popularity,
+        //                 //     vote_average: x.vote_average,
+        //                 //     vote_count: x.vote_count,
+        //                 //     key: x
+        //                 // }])
+        //             })
+        //         }
+        //         //  else {
+        //         //     toast.warn('You must search for a Show or a Movie!', {
+        //         //         autoClose: 2500
+        //         //     });
+        //         // }
+        //     });
     }
 
     return (
@@ -93,21 +115,20 @@ function Home() {
                         </button>
                     </div>
                 </form>
-                <div>
-                    {image ? image.map(x => (
+                {/* <div>
+                    {image ? image.map((x, y) => (
                         <div className="card" style={{ width: "18rem" }}>
-                            <img className="card-img-top" src={x} key={x} alt="Card" />
+                            <img className="card-img-top" src={x} key={y} alt="Card" />
                         </div>
                         // <img
                         //     key={x}
                         //     src={x}
                         //     alt='img'
                         // />
-                    ))
-                        : null}
-                </div>
+                    )) : null }
+                </div> */}
                 <div>
-                    {result ? result.map(x => (
+                    {result ? result.map((x, y) => (
                         // <Card
                         //     original_name={x.original_name}
                         //     overview={x.overview}
@@ -116,15 +137,20 @@ function Home() {
                         //     vote_count={x.vote_count}
                         //     key={x.key}
                         // />
-                        <div className="card bg-dark mb-3" style={{ width: "18rem" }} key={x}>
-                            <div className="card-body" key={x}>
-                                <h5 className="card-title text-center">{x.original_name}</h5>
-                                <p className="card-text text-center">{x.overview}</p>
-                                <p className="card-text text-center">Popularity: {x.popularity}</p>
-                                <p className="card-text text-center">Vote Average: {x.vote_average}</p>
-                                <p className="card-text text-center">Vote Count: {x.vote_count}</p>
+                        <>
+                            <div className='card' style={{ width: '18rem' }}>
+                                <img className="card-img-top" src={x.image} key={y} alt="Card" />
                             </div>
-                        </div>
+                            <div className="card bg-dark mb-3" style={{ width: "18rem" }} key={x}>
+                                <div className="card-body" key={x}>
+                                    <h5 className="card-title text-center">{x.original_name}</h5>
+                                    <p className="card-text text-center">{x.overview}</p>
+                                    <p className="card-text text-center">Popularity: {x.popularity}</p>
+                                    <p className="card-text text-center">Vote Average: {x.vote_average}</p>
+                                    <p className="card-text text-center">Vote Count: {x.vote_count}</p>
+                                </div>
+                            </div>
+                        </>
                     ))
                         : null}
                 </div>
