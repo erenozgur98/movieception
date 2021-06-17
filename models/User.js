1c87fa796df82e0125e4297d7bfd3b1b1488c599
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-06-16 20:35:53 -0400

    added bcrypt and will hash the passwords in the future

--- models/User.js
+++ models/User.js
@@ -1,23 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = requite('bcrypt');
 
-const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-        unique: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
+const userSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            unique: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
     }
-});
+);
 
 const User = mongoose.model('User', userSchema);
 
