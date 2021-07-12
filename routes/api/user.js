1b241a18cbc9ad49a8c808a6b7dcba220081a4b9
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-11 21:15:35 -0400

    added signup route

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,9 +3,11 @@ const User = require('../../models/User');
 
 router.get('/', async (req, res) => {
     try {
-
+        const user = await User.findAll();
+        res.json(user);
     } catch (err) {
-        
+        console.log(err);
+        res.status(400).json(err);
     }
 });
 
@@ -35,9 +37,23 @@ router.post('/login', async (req, res) => {
 
 router.post('/signup', async (req, res) => {
     try {
+        const email = await User.findOne({ where: { email: req.body.email }});
+        const user = await User.findOne({ where: { username: req.body.username}});
 
+        if (email) return res.status(400).json({ message: 'That email is taken' });
+        if (user) return res.status(400).json({ message: 'That username is taken' });
+
+        const newUser = await User.create(req.body);
+        delete newUser.password;
+
+        req.session.user_id = newUser._id;
+        req.session.logged_in = true;
+        req.session.username = newUser.username;
+
+        res.json(newUser);
     } catch (err) {
-
+        console.log(err);
+        res.status(404).json(err);
     }
 });
 
