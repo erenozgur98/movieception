8039ec17d87cb7eb960a1f3fed00269c3e01dfda
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-12 10:49:37 -0400

    added password verification for logging in

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -15,15 +15,15 @@ export default {
     },
 
     signUp: (userInfo) => {
-        return axios.post('/api/signup', userInfo);
+        return axios.post('/api/user/signup', userInfo);
     },
 
     logIn: (userInfo) => {
-        return axios.post('/api/login', userInfo);
+        return axios.post('/api/user/login', userInfo);
     },
 
     logOut: () => {
-        return axios.post('/api/logout');
+        return axios.post('/api/user/logout');
     },
 
     loggedIn: () => {
