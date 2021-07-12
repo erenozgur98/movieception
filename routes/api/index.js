d721ead7abd9c90ca24cd773b0e58971236b41cb
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-12 10:43:20 -0400

    small fixes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const userRoutes = require('./user');
 
 // User Routes
-router.use('/', userRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
