fccca0e7a9fea9f29e8fc3209026ada192206c18
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-11 21:09:28 -0400

    adding necessary code to the user.js

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,52 @@
 const router = require('express').Router();
 const User = require('../../models/User');
 
+router.get('/', async (req, res) => {
+    try {
 
+    } catch (err) {
+        
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+
+    } catch (err) {
+        
+    }
+});
+
+router.get('/user', async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+});
+
+router.post('/signup', async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+});
+
+router.post('/logout', async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+});
 
 module.exports = router;
