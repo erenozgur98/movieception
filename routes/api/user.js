d2bd45f9b4078d082ec80a0286a91e08cb9dd752
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-11 21:19:42 -0400

    added login route

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../../models/User');
+const bcrypt = require('bcrypt');
 
 router.get('/', async (req, res) => {
     try {
@@ -29,9 +30,28 @@ router.get('/user', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
+        const user = await User.findOne({ where: { username: req.body.username }});
+
+        if (!user) return res.status(403).json({ message: 'Incorrect Username' });
+
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+
+        if (!validPassword) return res.status(401).json({ message: 'Incorrect Password' });
+
+        const userData = JSON.parse(JSON.stringify(user));
+
+        req.session.user_id = userData._id;
+        req.session.logged_in = true;
+        req.session.username = userData.username;
+
+        res.json({ user: user });
 
     } catch (err) {
-
+        console.log('------------------------------------Line 53', err);
+        res.status(400).json(err);
     }
 });
 
@@ -52,7 +72,7 @@ router.post('/signup', async (req, res) => {
 
         res.json(newUser);
     } catch (err) {
-        console.log(err);
+        console.log('***************************** Line 75', err);
         res.status(404).json(err);
     }
 });
