63d1c77a7ecb25ca8572b50389c7d873bd03ad70
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-12 18:20:08 -0400

    added navbar modifications

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Navbar, Nav, NavbarBrand } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './style.css';
 
 function Header({ user, handleLogout }) {
@@ -17,58 +17,83 @@ function Header({ user, handleLogout }) {
     window.addEventListener('scroll', scrollHandler);
 
     return (
-        <Navbar
-            collapseOnSelect
-            expand='lg'
-            variant='dark'
-            sticky='top'
-            className={navColor ? 'navColor1' : 'navColor2'}
-        >
-            <NavbarBrand as={Link} className='navbar-brand' to='/'>
-                <img src='' className='logo' alt='logo' />
-            </NavbarBrand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className='ml-auto navbar'>
-                    <Nav.Link as={Link} to='/home' className='nav-link'>
-                        <div className="navLinks">
-                            Home
-                        </div>
-                    </Nav.Link>
-                    <Nav.Link as={Link} to='/login' className='nav-link'>
-                        <div className="navLinks">
-                            Login
-                        </div>
-                    </Nav.Link>
-                    {/* <Nav.Link as={Link} to='/discover' className='nav-link'>
-                        <div className="navLinks">
+        <div className="main-nav">
+            {user.username ? (
+                <Navbar
+                    collapseOnSelect
+                    expand='lg'
+                    variant='dark'
+                    sticky='top'
+                    className={navColor ? 'navColor1' : 'navColor2'}
+                >
+                    <NavbarBrand className='navbar-brand' to='/'>
+                        <img src='' className='logo' alt='logo' />
+                    </NavbarBrand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className='ml-auto'>
+                            <Nav.Link href='/home' className='nav-link'>
+                                Home
+                            </Nav.Link>
+                            {/* <Nav.Link href='/discover' className='nav-link'>
                             Discover
-                        </div>
                     </Nav.Link>
-                    <Nav.Link as={Link} to='/movies' className='nav-link'>
-                        <div className="navLinks">
+                    <Nav.Link href='/movies' className='nav-link'>
                             Movies
-                        </div>
                     </Nav.Link>
-                    <Nav.Link as={Link} to='/shows' className='nav-link'>
-                        <div className="navLinks">
+                    <Nav.Link href='/shows' className='nav-link'>
                             TV Shows
-                        </div>
                     </Nav.Link>
-                    <Nav.Link as={Link} to='/profile' className='nav-link'>
-                        <div className="navLinks">
+                    <Nav.Link href='/profile' className='nav-link'>
                             Profile
-                        </div>
                     </Nav.Link> */}
-                    <Nav.Link as={Link} to='/' className='nav-link' onClick={handleLogout}>
-                        <div className="navLinks">
-                            Logout
-                        </div>
+                            <Nav.Link href='/' className='nav-link' onClick={handleLogout}>
+                                Logout
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            ) : (
+                <Navbar
+                    collapseOnSelect
+                    expand='lg'
+                    variant='dark'
+                    sticky='top'
+                    className={navColor ? 'navColor1' : 'navColor2'}
+                >
+                    <NavbarBrand className='navbar-brand' to='/'>
+                        <img src='' className='logo' alt='logo' />
+                    </NavbarBrand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className='ml-auto'>
+                            <Nav.Link href='/home' className='nav-link'>
+                                Home
+                            </Nav.Link>
+                            <Nav.Link href='/login' className='nav-link'>
+                                Login
+                            </Nav.Link>
+                            {/* <Nav.Link href='/discover' className='nav-link'>
+                            Discover
                     </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+                    <Nav.Link href='/movies' className='nav-link'>
+                            Movies
+                    </Nav.Link>
+                    <Nav.Link href='/shows' className='nav-link'>
+                            TV Shows
+                    </Nav.Link>
+                    <Nav.Link href='/profile' className='nav-link'>
+                            Profile
+                    </Nav.Link> */}
+                            <Nav.Link href='/' className='nav-link' onClick={handleLogout}>
+                                Logout
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            )}
+        </div>
     )
-}
+};
 
 export default Header
