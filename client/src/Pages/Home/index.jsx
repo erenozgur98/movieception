560164eb7ec76ea4c27b3889137bc441f9126568
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-13 23:31:51 -0400

    small fixes

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -11,84 +11,83 @@ import { Container } from 'react-bootstrap'
 // Home will have random show or a movie(s) (3 in a row maybe) underneath the search bar( behind the search bar put the Barney picture )
 // Make a logo
 // Make toast work
-let IMGurl = 'https://image.tmdb.org/t/p/w300';
 
 function Home() {
-    // const [image, setImage] = useState([]);
-    // const [result, setResult] = useState([]);
-    // const searchedItem = useRef();
-    // // https://image.tmdb.org/t/p/original/
+    const [image, setImage] = useState([]);
+    const [result, setResult] = useState([]);
+    const searchedItem = useRef();
+    // https://image.tmdb.org/t/p/original/
 
-    // let fetchedResult = [];
-    // const clearImage = async () => {
-    //     // window.location.reload();
-    //     await setImage([]);
-    //     await setResult([]);
-    // }
+    let fetchedResult = [];
+    const clearImage = async () => {
+        // window.location.reload();
+        await setImage([]);
+        await setResult([]);
+    }
 
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     let baseUrl = 'https://api.themoviedb.org/3/';
-    //     let apiKey = 'af737f76cdba5b7435e17cc94568c07d';
-    //     let IMGurl = 'https://image.tmdb.org/t/p/w300';
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let baseUrl = 'https://api.themoviedb.org/3/';
+        let apiKey = 'af737f76cdba5b7435e17cc94568c07d';
+        let IMGurl = 'https://image.tmdb.org/t/p/w300';
 
-    //     let movieURL = `${baseUrl}search/movie/?api_key=${apiKey}&query=${searchedItem.current.value}`;
-    //     let tvURL = `${baseUrl}search/tv/?api_key=${apiKey}&query=${searchedItem.current.value}`;
+        let movieURL = `${baseUrl}search/movie/?api_key=${apiKey}&query=${searchedItem.current.value}`;
+        let tvURL = `${baseUrl}search/tv/?api_key=${apiKey}&query=${searchedItem.current.value}`;
 
-    //     await fetch(movieURL)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data)
-    //             fetchedResult = data;
-    //             console.log(fetchedResult);
-    //             if (searchedItem.current.value) {
-    //                 data.results.forEach(x => {
-    //                     // setImage([...image, { key: x }, `${IMGurl}${x.poster_path}`])
-    //                     setResult([...result, {
-    //                         image: `${IMGurl}${x.poster_path}`,
-    //                         original_name: x.original_name,
-    //                         overview: x.overview,
-    //                         popularity: x.popularity,
-    //                         vote_average: x.vote_average,
-    //                         vote_count: x.vote_count,
-    //                         key: x
-    //                     }])
-    //                     console.log(result);
-    //                 })
-    //             }
-    //             // else {
-    //             //     toast.warn('You must search for a Show or a Movie!', {
-    //             //         autoClose: 2500
-    //             //     });
-    //             // }
-    //         });
+        await fetch(movieURL)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                fetchedResult = data;
+                console.log(fetchedResult);
+                if (searchedItem.current.value) {
+                    data.results.forEach(x => {
+                        // setImage([...image, { key: x }, `${IMGurl}${x.poster_path}`])
+                        setResult([...result, {
+                            image: `${IMGurl}${x.poster_path}`,
+                            original_name: x.original_name,
+                            overview: x.overview,
+                            popularity: x.popularity,
+                            vote_average: x.vote_average,
+                            vote_count: x.vote_count,
+                            key: x
+                        }])
+                        console.log(result);
+                    })
+                }
+                // else {
+                //     toast.warn('You must search for a Show or a Movie!', {
+                //         autoClose: 2500
+                //     });
+                // }
+            });
 
-    //     await fetch(tvURL)
-    //         .then(res => res.json())
-    //         .then(async res => {
-    //             console.log(res)
-    //             if (searchedItem.current.value) {
-    //                 await res.results.forEach(x => {
-    //                     console.log(result);
-    //                     console.log(image);
-    //                     setImage([...image, `${IMGurl}${x.poster_path}`])
-    //                     // setResult([...result, {
-    //                     //     original_name: x.original_name,
-    //                     //     overview: x.overview,
-    //                     //     popularity: x.popularity,
-    //                     //     vote_average: x.vote_average,
-    //                     //     vote_count: x.vote_count,
-    //                     //     key: x
-    //                     // }])
-    //                 })
-    //             }
-    //             //  else {
-    //             //     toast.warn('You must search for a Show or a Movie!', {
-    //             //         autoClose: 2500
-    //             //     });
-    //             // }
-    //         });
-    // }
+        await fetch(tvURL)
+            .then(res => res.json())
+            .then(async res => {
+                console.log(res)
+                if (searchedItem.current.value) {
+                    await res.results.forEach(x => {
+                        console.log(result);
+                        console.log(image);
+                        setImage([...image, `${IMGurl}${x.poster_path}`])
+                        // setResult([...result, {
+                        //     original_name: x.original_name,
+                        //     overview: x.overview,
+                        //     popularity: x.popularity,
+                        //     vote_average: x.vote_average,
+                        //     vote_count: x.vote_count,
+                        //     key: x
+                        // }])
+                    })
+                }
+                //  else {
+                //     toast.warn('You must search for a Show or a Movie!', {
+                //         autoClose: 2500
+                //     });
+                // }
+            });
+    }
 
     return (
         <div>
@@ -97,7 +96,7 @@ function Home() {
                 className="d-flex align-items-center flex-column"
             >
                 <form
-                    // onSubmit={handleSubmit}
+                    onSubmit={handleSubmit}
                 >
                     <div className="input-group">
                         <div className="form-outline">
@@ -105,13 +104,13 @@ function Home() {
                                 type="search"
                                 id="form1"
                                 className="form-control"
-                                // ref={searchedItem}
+                                ref={searchedItem}
                             />
                         </div>
                         <button
                             type="button"
                             className="btn btn-primary"
-                            // onClick={handleSubmit}
+                            onClick={handleSubmit}
                         >
                             Search
                         </button>
@@ -155,7 +154,7 @@ function Home() {
                         </>
                     ))} */}
                 </div>
-                {/* <div>
+                <div>
                     {result ? result.map((x, y) => (
                         // <Card
                         //     original_name={x.original_name}
@@ -181,11 +180,11 @@ function Home() {
                         </>
                     ))
                         : null}
-                </div> */}
+                </div>
                 <button
                     type='button'
                     className='btn btn-primary'
-                    // onClick={clearImage}
+                    onClick={clearImage}
                 >
                     Clear
                 </button>
