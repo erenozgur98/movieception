3048c4a1934ce1829a7569634f01734d2fd524d4
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-08-24 00:25:33 -0400

    small updates on shows

--- client/src/Pages/Shows/index.jsx
+++ client/src/Pages/Shows/index.jsx
@@ -5,21 +5,21 @@ import requests from '../../components/Requests';
 function Shows() {
     return (
         <div className='show'>
-            <Row fetchUrl={requests.fetchNetflixOriginals} title='Netflix' />
+            {/* <Row fetchUrl={requests.fetchNetflixOriginals} title='Netflix' /> */}
             <Row fetchUrl={requests.fetchTrendingShows} title='Trending Shows' />
-            <Row fetchUrl={requests.fetchActionShows} title='Action Shows' />
-            <Row fetchUrl={requests.fetchComedyShows} title='Comedy Shows' />
-            <Row fetchUrl={requests.fetchRomanceShows} title='Romance Shows' />
-            <Row fetchUrl={requests.fetchDocumentariesShows} title='Documentary Shows' />
-            <Row fetchUrl={requests.fetchDramaShows} title='Drama Shows' />
-            <Row fetchUrl={requests.fetchAnimationShows} title='Animation Shows' />
-            <Row fetchUrl={requests.fetchCrimeShows} title='Crime Shows' />
-            <Row fetchUrl={requests.fetchFamilyShows} title='Family Shows' />
-            <Row fetchUrl={requests.fetchFantasyShows} title='Fantasy Shows' />
-            <Row fetchUrl={requests.fetchTalkShows} title='Talk Shows' />
-            <Row fetchUrl={requests.fetchRealityShows} title='Reality Shows' />
-            <Row fetchUrl={requests.fetchMysteryShows} title='Mystery Shows' />
-            <Row fetchUrl={requests.fetchWarShows} title='War Shows' />
+            {/* <Row fetchUrl={requests.fetchActionShows} title='Action Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchComedyShows} title='Comedy Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchRomanceShows} title='Romance Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchDocumentariesShows} title='Documentary Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchDramaShows} title='Drama Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchAnimationShows} title='Animation Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchCrimeShows} title='Crime Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchFamilyShows} title='Family Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchFantasyShows} title='Fantasy Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchTalkShows} title='Talk Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchRealityShows} title='Reality Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchMysteryShows} title='Mystery Shows' /> */}
+            {/* <Row fetchUrl={requests.fetchWarShows} title='War Shows' /> */}
         </div>
     )
 }
