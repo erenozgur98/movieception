9875454a98fce3836e89e7ab0c4024b6d6d7a454
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-08-26 20:53:56 -0400

    created css file for movie page, added some dummy data to play around with

--- client/src/Pages/MoviePage/index.jsx
+++ client/src/Pages/MoviePage/index.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import API from '../../utils/API';
+import "./MoviePage.css"
 
 function MoviePage() {
     const [movie, setMovie] = useState({});
@@ -13,7 +14,7 @@ function MoviePage() {
     }, [MovieId])
 
     return (
-        <Container>
+        <div>
             {/* banner, has movie's backdrop path */}
 
             {/* actual movie picture */}
@@ -34,7 +35,29 @@ function MoviePage() {
             {/* video link type: youtube.com/watch?v=${key} <-- key being the video link key from the api */}
 
             {/* actors, with names */}
-        </Container>
+            <section className="top-section">
+                <div className="background-picture">
+                    {/* change w500 / w200 to original after styling */}
+                    <img src="https://image.tmdb.org/t/p/original/dq18nCTTLpy9PmtzZI6Y2yAgdw5.jpg" alt="black-widow" className='movie-background' />
+                </div>
+                <div className="poster-picture">
+                    <img src="https://image.tmdb.org/t/p/original/qAZ0pzat24kLdO3o8ejmbLxyOac.jpg" alt="black-widow" className='movie-poster' />
+                    <h2>Black Widow</h2>
+                </div>
+            </section>
+            <section className="bottom-section">
+                <div className="movie-overview">
+                    <p>
+                        release_date: 2021-07-07
+                        genre_ids: [ 28, 12, 53, 878 ]
+                        original_language: "en"
+                    </p>
+                    <p>
+                        Natasha Romanoff, also known as Black Widow, confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by a force that will stop at nothing to bring her down, Natasha must deal with her history as a spy and the broken relationships left in her wake long before she became an Avenger.
+                    </p>
+                </div>
+            </section>
+        </div>
     )
 }
 
