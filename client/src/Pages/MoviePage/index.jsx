93f8779e3315bdc8e66ccead127b94d0b27cff1a
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-08-29 13:23:31 -0400

    wokring on movie page, coming along

--- client/src/Pages/MoviePage/index.jsx
+++ client/src/Pages/MoviePage/index.jsx
@@ -3,20 +3,26 @@ import { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import API from '../../utils/API';
+import axios from '../../components/Axios';
 import "./MoviePage.css"
 
 function MoviePage() {
     const [movie, setMovie] = useState({});
     const { MovieId } = useParams();
 
+    const apiKey = 'af737f76cdba5b7435e17cc94568c07d';
+
     useEffect(() => {
         // API.getOneMovie, will be set later
-        setMovie(MovieId.data)
-        console.log(MovieId)
+        const fetchData = async () => {
+            const request = await axios.get(`/movie/${MovieId}?api_key=${apiKey}`);
+            setMovie(request.data)
+        }
+        fetchData();
     }, [MovieId])
-    
+
     console.log(movie)
-    
+
     return (
         <div>
             {/* banner, has movie's backdrop path */}
@@ -41,23 +47,59 @@ function MoviePage() {
             {/* actors, with names */}
             <section className="top-section">
                 <div className="background-picture">
-                    <img src="https://image.tmdb.org/t/p/original/dq18nCTTLpy9PmtzZI6Y2yAgdw5.jpg" alt="black-widow" className='movie-background' />
+                    <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt="movie-poster" className='movie-background' />
                 </div>
                 <div className="poster-picture">
-                    <img src="https://image.tmdb.org/t/p/original/qAZ0pzat24kLdO3o8ejmbLxyOac.jpg" alt="black-widow" className='movie-poster' />
-                    <h2>Black Widow</h2>
+                    <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="black-widow" className='movie-poster' />
+                    <h2>{movie.original_title}</h2>
                 </div>
             </section>
             <section className="bottom-section">
                 <div className="movie-overview">
-                    <p>
-                        release_date: 2021-07-07
-                        genre_ids: [ 28, 12, 53, 878 ]
-                        original_language: "en"
-                    </p>
-                    <p>
-                        Natasha Romanoff, also known as Black Widow, confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by a force that will stop at nothing to bring her down, Natasha must deal with her history as a spy and the broken relationships left in her wake long before she became an Avenger.
-                    </p>
+                    <div>
+                        <a href={movie?.homepage}>Homepage</a>
+                    </div>
+                    <div>
+                        Release Date: {movie?.release_date}
+                    </div>
+                    <div>
+                        Genres: {movie?.genres.map(x => x.name)}
+                    </div>
+                    <div>
+                        Language: {movie?.original_language}
+                    </div>
+                    <div>
+                        Spoken Languages: {movie?.spoken_languages.map(x => x.english_name)}
+                    </div>
+                    <div>
+                        Runtime: {movie?.runtime} minutes
+                    </div>
+                    <div>
+                        Vote Average: {movie?.vote_average} / 10
+                    </div>
+                    <div>
+                        Budget: {movie?.budget}
+                    </div>
+                    <div>
+                        Revenue: {movie?.revenue}
+                    </div>
+                    <div>
+                        Country: {movie?.production_countries.map(x => x.name)}
+                    </div>
+                    <div>
+                        {/* add logos to companies later */}
+                        Production Companies: {movie?.production_companies.map(x => x.name)}
+                    </div>
+                    <div>
+                        {/* maybe add this too? will consider */}
+                        {/* {movie?.belongs_to_collection} */}
+                    </div>
+                    <div>
+                        {movie?.tagline}
+                    </div>
+                    <div>
+                        {movie?.overview}
+                    </div>
                 </div>
             </section>
         </div>
