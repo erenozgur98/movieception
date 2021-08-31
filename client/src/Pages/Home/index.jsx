f9fe638445da6d8b175b2c45a863f59046f4ee2a
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-08-31 01:29:45 -0400

    cleaned out home page for further development

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -1,59 +1,13 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react';
-import { useRef } from 'react';
 import { Container } from 'react-bootstrap'
-import { useHistory } from 'react-router-dom';
-// import MovieDetail from '../../components/MovieDetail';
-// import { toast } from 'react-toastify';
-import requests from '../../components/Requests';
-// import Banner from '../../components/Banner';
-// import Details from '../../components/Details';
-import Row from '../../components/Row';
 
 function Home({ user }) {
-    const [result, setResult] = useState([]);
-    // const [randomMovie, setRandomMovie] = useState({});
-    // const [randomShow, setRandomShow] = useState({});
     const [currentUser, setCurrentUser] = useState({});
-
-    const search = useRef();
-    const history = useHistory();
 
     useEffect(() => {
         if (user) setCurrentUser(user.username)
     }, [user]);
-
-
-    // const randomNumbers = Math.floor(100000 + Math.random() * 9000000);
-
-    const handleSearch = () => {
-        const searchedMovie = search.current.value
-        setResult(searchedMovie)
-        history.push(`/search/query=${searchedMovie}`);
-    }
-
-    const handleSearchEnter = (e) => {
-        if (e.charCode === 13) {
-            const searchedMovie = search.current.value
-            setResult(searchedMovie)
-            history.push(`/search/query=${searchedMovie}`);
-        }
-    };
-
-    const handleRandomMovie = () => {
-        setResult([]);
-        // setRandomShow([]);
-    };
-
-    const handleRandomShow = () => {
-        setResult([]);
-        // setRandomMovie([]);
-    };
-
-    const clear = () => {
-        setResult([])
-        window.location.reload();
-    };
 
     return (
         <div>
@@ -68,43 +22,6 @@ function Home({ user }) {
                     )}
                 </div>
             </Container>
-            <Container className="d-flex justify-content-center">
-                <div>
-                    <div className="form-outline">
-                        <input
-                            type="search"
-                            className="form-control rounded"
-                            placeholder="Search"
-                            onKeyPress={handleSearchEnter}
-                            ref={search}
-                        />
-                    </div>
-                </div>
-                <button type='button' className='btn btn-primary' onClick={handleSearch}>
-                    <i className="fas fa-search"></i>
-                </button>
-            </Container>
-            {/* <Container className="d-flex justify-content-center">
-                <div>
-                    <button type='button' className='btn btn-warning' onClick={handleRandomMovie}>
-                        Random Movie
-                    </button>
-                    <button type='button' className='btn btn-warning' onClick={handleRandomShow}>
-                        Random Show
-                    </button>
-                </div>
-            </Container> */}
-            <Container className="d-flex justify-content-center">
-                <div>
-                    <button type='button' className='btn btn-danger' onClick={clear}>
-                        Clear
-                    </button>
-                </div>
-            </Container>
-            <div>
-                {/* <MovieDetail fetchUrl={`${requests.fetchSearchMulti}${result}`} /> */}
-                {result && <Row fetchUrl={`${requests.fetchSearchMulti}${result}`} title='Search Results' />}
-            </div>
         </div>
     )
 }
