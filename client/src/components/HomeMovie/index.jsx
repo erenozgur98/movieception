372809e8ad295ee8f7613d6ecda0b2f24627225e
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-09-01 11:47:53 -0400

    small updates on css

--- client/src/components/HomeMovie/index.jsx
+++ client/src/components/HomeMovie/index.jsx
@@ -24,18 +24,23 @@ function HomeMovie({ fetchUrl }) {
 
     return (
         <div className='home-movie'>
-            <img
-                onClick={handleClick}
-                className='home-movie-poster'
-                src={movies[count]?.poster_path ?
-                    `${base_url}${movies[count]?.poster_path}`
-                    :
-                    "https://via.placeholder.com/300"
-                }
-                alt={movies[count]?.name}
-            />
-            <div onClick={() => setCount(count + 1)}>ARROW +</div>
-            <div onClick={() => setCount(count - 1)}>ARROW -</div>
+            {/* <div
+                className='home-movie-background'
+                style={{ backgroundImage: `url(${base_url}${movies[count]?.backdrop_path})` }}
+            > */}
+                <img
+                    onClick={handleClick}
+                    className='home-movie-poster'
+                    src={movies[count]?.poster_path ?
+                        `${base_url}${movies[count]?.poster_path}`
+                        :
+                        "https://via.placeholder.com/300"
+                    }
+                    alt={movies[count]?.name}
+                />
+                <div onClick={() => setCount(count + 1)}>ARROW +</div>
+                <div onClick={() => setCount(count - 1)}>ARROW -</div>
+            {/* </div> */}
         </div>
     );
 };
