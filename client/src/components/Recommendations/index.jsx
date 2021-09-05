f0f7cd0e24e0f39444b2e38f70a338dba4ce9bdf
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-09-05 01:58:50 -0400

    updates on recommendation titles

--- client/src/components/Recommendations/index.jsx
+++ client/src/components/Recommendations/index.jsx
@@ -49,7 +49,7 @@ function Recommendations({ movie, show }) {
                         null
                         :
                         <div>
-                            <div>Since you are looking at {movie?.original_title}, you might like these titles:</div>
+                            <div>Since you are looking at {movie?.original_title}, you might like these movies:</div>
                             <div className="recommendations-posters">
                                 {movieRecommendations?.map((recommendations) => (
                                     <div className='recommendations-map'>
@@ -71,7 +71,7 @@ function Recommendations({ movie, show }) {
                         null
                         :
                         <div>
-                            <div>Since you are looking at {show?.name}, you might like these titles:</div>
+                            <div>Since you are looking at {show?.name}, you might like these shows:</div>
                             <div className="recommendations-posters">
                                 {showRecommendations?.map((recommendations) => (
                                     <div className='recommendations-map'>
