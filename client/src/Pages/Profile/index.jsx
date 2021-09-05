0b90fd669a7440f39db50c7086e6fbfafd466272
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-09-05 19:35:05 -0400

    updated profile page, it's now under construction

--- client/src/Pages/Profile/index.jsx
+++ client/src/Pages/Profile/index.jsx
@@ -4,38 +4,8 @@ import { Container } from 'react-bootstrap';
 function Profile() {
     return (
         <>
-            <Container>
-                <div className="card bg-dark">
-                    <img className="card-img-top" src="" alt="Card" />
-                    <div className="card-body">
-                        <h4 className="card-title">Some Example Header</h4>
-                        <p className="card-text">Some example text</p>
-                        <a href="/" className="btn btn-danger">See Profile</a>
-                    </div>
-                </div>
-                <h4>Some Header</h4>
-                <div className="card-deck">
-                    <div className="card bg-primary">
-                        <div className="card-body text-center">
-                            <p className="card-text"></p>
-                        </div>
-                    </div>
-                    <div className="card bg-warning">
-                        <div className="card-body text-center">
-                            <p className="card-text"></p>
-                        </div>
-                    </div>
-                    <div className="card bg-success">
-                        <div className="card-body text-center">
-                            <p className="card-text"></p>
-                        </div>
-                    </div>
-                    <div className="card bg-danger">
-                        <div className="card-body text-center">
-                            <p className="card-text"></p>
-                        </div>
-                    </div>
-                </div>
+            <Container style={{ paddingTop: '80px' }}>
+                <h1 className='d-flex justify-content-center text-center'>Page under construction, hang tight and you'll have your profile page!</h1>
             </Container>
         </>
     )
