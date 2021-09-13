b1d0d3d1dc96a8af1fb405d28315469d6d01a7d3
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-09-12 22:12:18 -0400

    working on season/episodes page

--- client/src/Pages/SeasonPage/index.jsx
+++ client/src/Pages/SeasonPage/index.jsx
@@ -1,35 +1,34 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import axios from '../../components/Axios';
 import './SeasonPage.css';
+import Banner from '../../components/Banner';
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 const apiKey = 'af737f76cdba5b7435e17cc94568c07d';
 
 function SeasonPage() {
+    const [show, setShow] = useState({});
     const [credits, setCredits] = useState();
-    const [season, setSeason] = useState(1);
     const { SeasonId } = useParams();
     const { ShowId } = useParams();
 
     useEffect(() => {
         const fetchData = async () => {
             const requestCredits = await axios.get(`tv/${ShowId}/season/${SeasonId}/aggregate_credits?api_key=${apiKey}`);
+            const request = await axios.get(`/tv/${ShowId}?api_key=${apiKey}`);
             setCredits(requestCredits.data);
-            setSeason(SeasonId);
+            setShow(request.data.seasons[SeasonId]);
         }
         fetchData();
     }, [SeasonId, ShowId]);
 
-    console.log(credits)
-
-   // https://api.themoviedb.org/3/tv/63174/season/5/aggregate_credits?api_key=af737f76cdba5b7435e17cc94568c07d&language=en-US
+    console.log('sssssssssssssssss', show)
 
     return (
         <div>
-            <h1>Page Under Construction</h1>
+            {/* show returns name, overview, air_date, episode_count, id, poster_path, season_number */}
         </div>
     )
 }
