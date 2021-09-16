28c22888065e2b8e886597d53691b13555b9237b
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-09-16 11:57:38 -0400

    code cleaning

--- client/src/components/Requests/index.jsx
+++ client/src/components/Requests/index.jsx
@@ -46,10 +46,6 @@ const requests = {
 
     // fetching actors
     fetchActors : `/person/popular?api_key=${apiKey}`,
-    
-    // getting the episodes for tv shows => 
-    // https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}?api_key={apiKey}
-
 
     // searching multi; shows, movies, actors
     fetchSearchMulti: `/search/multi?api_key=${apiKey}&language=en-US&query=`,
@@ -62,8 +58,3 @@ const requests = {
 }
 
 export default requests;
-
-// https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=${language}&query=${query}&page=1&include_adult=false
-
-// https://api.themoviedb.org/3/discover/tv?api_key=af737f76cdba5b7435e17cc94568c07d&with_genres=28
-// https://api.themoviedb.org/3/discover/tv?api_key=af737f76cdba5b7435e17cc94568c07d&with_genres=
