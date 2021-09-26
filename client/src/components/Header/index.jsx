f94c7eecfeb66c53ece0fd06b14cab48c67b7491
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-09-25 20:37:36 -0400

    re-creating the header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -58,42 +58,50 @@ function Header({ user, handleLogout }) {
                     </Navbar.Collapse>
                 </Navbar>
             ) : (
-                <Navbar
-                    collapseOnSelect
-                    expand='lg'
-                    variant='dark'
-                    sticky='top'
-                    className={navColor ? 'navColor2' : 'navColor1'}
-                >
-                    <NavbarBrand href='/'>
-                        <h4>True Story</h4>
-                    </NavbarBrand>
-                    <SearchForm />
-                    {/* <NavbarBrand className='navbar-brand' href='/'>
-                        <img src='' className='logo' alt='logo' />
-                    </NavbarBrand> */}
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className='ml-auto'>
-                            <Nav.Link href='/home' className='nav-link' style={{ color: 'white' }}>
-                                Home
-                            </Nav.Link>
-                            <Nav.Link href='/discover' className='nav-link' style={{ color: 'white' }}>
-                                Discover
-                            </Nav.Link>
-                            <Nav.Link href='/actors' className='nav-link' style={{ color: 'white' }}>
-                                Actors
-                            </Nav.Link>
-                            <Nav.Link href='/login' className='nav-link' style={{ color: 'white' }}>
-                                Login
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-
-                </Navbar>
+                <div className='header'>
+                    <a href='/' className='link'>Home</a>
+                    <div className="dropdown">
+                        <button className='link'>Discover</button>
+                        <div className="dropdown-menus">
+                            Dropdown Content
+                        </div>
+                    </div>
+                </div>
             )}
         </div>
     )
 };
 
 export default Header
+
+
+{/* <Navbar
+collapseOnSelect
+expand='lg'
+variant='dark'
+sticky='top'
+className={navColor ? 'navColor2' : 'navColor1'}
+>
+<NavbarBrand href='/'>
+    <h4>True Story</h4>
+</NavbarBrand>
+<SearchForm />
+<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+<Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className='ml-auto'>
+        <Nav.Link href='/home' className='nav-link' style={{ color: 'white' }}>
+            Home
+        </Nav.Link>
+        <Nav.Link href='/discover' className='nav-link' style={{ color: 'white' }}>
+            Discover
+        </Nav.Link>
+        <Nav.Link href='/actors' className='nav-link' style={{ color: 'white' }}>
+            Actors
+        </Nav.Link>
+        <Nav.Link href='/login' className='nav-link' style={{ color: 'white' }}>
+            Login
+        </Nav.Link>
+    </Nav>
+</Navbar.Collapse>
+
+</Navbar> */}
