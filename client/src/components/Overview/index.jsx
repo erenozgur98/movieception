95f917cf04a18cd4d4bd6463d704f7574fbdf18f
Yigit Ozgur <yigiterenozgur@outlook.com>
2022-02-15 22:55:49 -0500

    Added new information to the TV Shows Page

--- client/src/components/Overview/index.jsx
+++ client/src/components/Overview/index.jsx
@@ -36,48 +36,92 @@ const Overview = ({ link }) => {
     };
 
     const date = new Date(link?.release_date || link?.first_air_date);
+    const nextEpisodeDate = new Date(link?.next_episode_to_air?.air_date);
+    const lastEpisodeDate = new Date(link?.last_episode_to_air?.air_date);
+
     return (
         <div>
             {/* Top Section */}
             <h1 style={{ textAlign: 'center' }}>{link?.original_title || link?.name}</h1>
+
             <StyledDivCenter style={{ fontStyle: "italic", padding: '12px 0px' }}>
                 {link?.tagline && <div>{link?.tagline}</div>}
             </StyledDivCenter>
+
             <StyledDivCenter>
                 {link?.genres?.map(x => <StyledChip label={x?.name} color='warning' />)}
             </StyledDivCenter>
+
             <StyledDivPadding style={{ textAlign: 'center', marginBottom: '2rem', marginTop: '1rem' }}>
                 {link?.overview && <div>{link?.overview}</div>}
             </StyledDivPadding>
 
+            <div style={{ marginBottom: '2rem' }}>
+                {link?.last_episode_to_air &&
+                    <StyledDivPadding style={{ textAlign: 'center' }}>
+                        Last Episode To Air: <StyledSpan>Season {link?.last_episode_to_air?.season_number} Episode {link?.last_episode_to_air?.episode_number}</StyledSpan> in <StyledSpan>{lastEpisodeDate.toLocaleString('en-US', options)}</StyledSpan>
+                    </StyledDivPadding>
+                }
+                {link?.next_episode_to_air &&
+                    <StyledDivPadding style={{ textAlign: 'center' }}>
+                        Next Episode To Air: <StyledSpan>Season {link?.next_episode_to_air?.season_number} Episode {link?.next_episode_to_air?.episode_number}</StyledSpan> in <StyledSpan>{nextEpisodeDate.toLocaleString('en-US', options)}</StyledSpan>
+                    </StyledDivPadding>
+                }
+            </div>
+
+            <StyledDivPadding style={{ textAlign: 'center' }}>
+                {link?.status && <div>Status: <StyledSpan>{link?.status}</StyledSpan></div>}
+            </StyledDivPadding>
+
             {/* Bottom section */}
             <StyledBottomSection>
-                {date && <StyledDivInBottomSection>
-                    Release Date: <StyledSpan>{date.toLocaleString('en-US', options)}</StyledSpan>
-                </StyledDivInBottomSection>}
-                {link?.original_language === 'en' && <StyledDivInBottomSection>
-                    Language: <StyledSpan>English</StyledSpan>
-                </StyledDivInBottomSection>}
-                {link?.runtime && <StyledDivInBottomSection>
-                    Runtime: <StyledSpan>{link?.runtime} minutes</StyledSpan>
-                </StyledDivInBottomSection>}
-                {link?.budget > 0 && <StyledDivInBottomSection>
-                    Budget: <StyledSpan>{link?.budget.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</StyledSpan>
-                </StyledDivInBottomSection>}
-                {link?.revenue > 0 && <StyledDivInBottomSection>
-                    Revenue: <StyledSpan>{link?.revenue.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</StyledSpan>
-                </StyledDivInBottomSection>}
-                {link?.production_countries && <StyledDivInBottomSection>
-                    Country: <StyledSpan>{link?.production_countries[0]?.name}</StyledSpan>
-                </StyledDivInBottomSection>}
+                {date &&
+                    <StyledDivInBottomSection>
+                        Release Date: <StyledSpan>{date.toLocaleString('en-US', options)}</StyledSpan>
+                    </StyledDivInBottomSection>
+                }
+                {link?.original_language === 'en' &&
+                    <StyledDivInBottomSection>
+                        Language: <StyledSpan>English</StyledSpan>
+                    </StyledDivInBottomSection>
+                }
+                {link?.runtime &&
+                    <StyledDivInBottomSection>
+                        Runtime: <StyledSpan>{link?.runtime} minutes</StyledSpan>
+                    </StyledDivInBottomSection>
+                }
+                {link?.budget > 0 &&
+                    <StyledDivInBottomSection>
+                        Budget: <StyledSpan>{link?.budget.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</StyledSpan>
+                    </StyledDivInBottomSection>
+                }
+                {link?.revenue > 0 &&
+                    <StyledDivInBottomSection>
+                        Revenue: <StyledSpan>{link?.revenue.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</StyledSpan>
+                    </StyledDivInBottomSection>
+                }
+                {link?.production_countries &&
+                    <StyledDivInBottomSection>
+                        Country: <StyledSpan>{link?.production_countries[0]?.name}</StyledSpan>
+                    </StyledDivInBottomSection>
+                }
                 {/* add logos to companies later */}
-                {link?.production_companies && <StyledDivInBottomSection>
-                    Production Company: <StyledSpan>{link?.production_companies[0]?.name}</StyledSpan>
-                </StyledDivInBottomSection>}
+                {link?.production_companies &&
+                    <StyledDivInBottomSection>
+                        Production Company: <StyledSpan>{link?.production_companies[0]?.name}</StyledSpan>
+                    </StyledDivInBottomSection>
+                }
                 {/* maybe add this too? will consider */}
-                {link?.belongs_to_collection && <StyledDivInBottomSection>
-                    Collection: <StyledSpan>{link?.belongs_to_collection?.name}</StyledSpan>
-                </StyledDivInBottomSection>}
+                {link?.belongs_to_collection &&
+                    <StyledDivInBottomSection>
+                        Collection: <StyledSpan>{link?.belongs_to_collection?.name}</StyledSpan>
+                    </StyledDivInBottomSection>
+                }
+                {link?.number_of_episodes &&
+                    <StyledDivInBottomSection>
+                        Total Episodes: <StyledSpan>{link?.number_of_episodes}</StyledSpan>
+                    </StyledDivInBottomSection>
+                }
             </StyledBottomSection>
         </div>
     )
