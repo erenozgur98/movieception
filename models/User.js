4983b365d8a2f256f17d4a6e67697b73490adda4
Yigit Ozgur <yigiterenozgur@outlook.com>
2022-03-01 13:18:01 -0500

    Updated Modal

--- models/User.js
+++ models/User.js
@@ -50,11 +50,6 @@ const UserSchema = new Schema(
             {
                 type: Number,
             }
-        ],
-        actorFavorites: [
-            {
-                type: Number,
-            }
         ]
     }
 );
