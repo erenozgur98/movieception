70db18852ddd4c6b86331e57989fba9e3928966a
Yiğit Eren Özgür <yigiterenozgur@outlook.com>
2022-06-29 21:07:03 -0400

    Styled Lists

--- client/src/components/Lists/Favorites.js
+++ client/src/components/Lists/Favorites.js
@@ -2,53 +2,79 @@ import React from "react";
 import { styled } from '@mui/material/styles';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FolderIcon from '@mui/icons-material/Folder';
-import { Grid, Typography, List, ListItem, ListItemAvatar, ListItemText, Avatar, IconButton, Checkbox } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import { Grid, Typography, List, ListItem, ListItemAvatar, ListItemText, ListItemButton, Avatar, IconButton, Checkbox, Collapse } from '@mui/material';
 import './index.css'
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
 const Favorites = ({ favoriteMovies, favoriteShows }) => {
+    const [open, setOpen] = React.useState(false);
+
     const Demo = styled('div')(({ theme }) => ({
         backgroundColor: 'rgb(116, 15, 15)',
         borderRadius: '15px'
     }));
 
+    const handleClick = () => {
+        setOpen(!open);
+    }
+
     const redirect = id => {
+        window.location.assign(`/movies/${id}`)
+    }
+
+    const remove = id => {
         console.log(id)
-        // window.location.assign('')
     }
 
     return (
         <>
-            <Grid item xs={12} md={12} style={{ margin: '0rem 2rem' }}>
-                <Demo>
-                    <Typography variant="h6" style={{ padding: '1rem 0rem', textDecoration: 'underline' }}>
-                        Favorites
-                    </Typography>
-                    {favoriteMovies?.map(x => (
-                        <List>
-                            <ListItem
-                                secondaryAction={
-                                    <IconButton edge="end" aria-label="delete">
-                                        <DeleteIcon />
-                                    </IconButton>
-                                }
-                            >
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        {x.poster_path ? <Avatar src={`${base_url}${x.poster_path}`} /> : <FolderIcon />}
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText
-                                    primary={x.title}
-                                />
-                            </ListItem>
-                        </List>
-                    ))}
-                </Demo>
-            </Grid>
+            <div className='main-grid-component'>
+                <Grid item xs={12} md={12}>
+                    <Demo>
+                        <Typography variant="h6" style={{ padding: '1rem 0rem', textDecoration: 'underline' }}>
+                            Favorites
+                        </Typography>
+                        <ListItemButton onClick={handleClick} >
+                            <ListItemIcon>
+                                <FavoriteIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Favorites" />
+                            {open ? <ExpandLess /> : <ExpandMore />}
+                        </ListItemButton>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            {favoriteMovies.map(x => (
+                                <List disablePadding>
+                                    <ListItem
+                                        secondaryAction={
+                                            <IconButton edge="end" aria-label="delete" onClick={() => remove(x.id)}>
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        }
+                                    >
+                                        <ListItemButton onClick={() => redirect(x.id)}>
+                                            <ListItemAvatar>
+                                                <Avatar>
+                                                    {x.poster_path ? <Avatar src={`${base_url}${x.poster_path}`} /> : <FolderIcon />}
+                                                </Avatar>
+                                            </ListItemAvatar>
+                                            <ListItemText
+                                                primary={x.title}
+                                            />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </List>
+                            ))}
+                        </Collapse>
+                    </Demo>
+                </Grid>
+            </div>
         </>
     )
-}
+} 
 
 export default Favorites
--- client/src/components/Lists/WatchList.js
+++ client/src/components/Lists/WatchList.js
@@ -2,18 +2,17 @@ import React from "react";
 import { styled } from '@mui/material/styles';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FolderIcon from '@mui/icons-material/Folder';
-import { Grid, Typography, List, ListItem, ListItemAvatar, ListItemText, ListItemButton, Avatar, IconButton, Checkbox, Collapse } from '@mui/material';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import StarBorder from '@mui/icons-material/StarBorder';
+import PlaylistAddCheckRoundedIcon from '@mui/icons-material/PlaylistAddCheckRounded';
+import { Grid, Typography, List, ListItem, ListItemAvatar, ListItemText, ListItemButton, Avatar, IconButton, Checkbox, Collapse } from '@mui/material';
 import './index.css'
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
 const WatchList = ({ movieWatchList, showWatchList }) => {
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = React.useState(false);
 
     const handleClick = () => {
         setOpen(!open);
@@ -23,58 +22,56 @@ const WatchList = ({ movieWatchList, showWatchList }) => {
         window.location.assign(`/movies/${id}`)
     }
 
+    const remove = id => {
+        console.log(id)
+    }
+
     const Demo = styled('div')(({ theme }) => ({
         backgroundColor: 'rgb(203, 193, 50)',
         borderRadius: '15px'
     }));
     return (
         <>
-            <Grid item xs={12} md={12} style={{ margin: '0rem 2rem' }}>
-                <Demo>
-                    <Typography variant="h6" style={{ padding: '1rem 0rem', textDecoration: 'underline' }}>
-                        WatchList
-                    </Typography>
-                    <ListItemButton onClick={handleClick}>
-                        <ListItemIcon>
-                            <InboxIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Inbox" />
-                        {open ? <ExpandLess /> : <ExpandMore />}
-                    </ListItemButton>
-                    {movieWatchList.map(x => (
-                        <List>
-                            <ListItemButton onClick={() => redirect(x.id)}>
-                                <ListItem
-                                    secondaryAction={
-                                        <IconButton edge="end" aria-label="delete">
-                                            <DeleteIcon />
-                                        </IconButton>
-                                    }
-                                >
-                                    <ListItemAvatar>
-                                        <Avatar>
-                                            {x.poster_path ? <Avatar src={`${base_url}${x.poster_path}`} /> : <FolderIcon />}
-                                        </Avatar>
-                                    </ListItemAvatar>
-                                    <ListItemText
-                                        primary={x.title}
-                                    />
-                                </ListItem>
-                            </ListItemButton>
-                            <Collapse in={open} timeout="auto" unmountOnExit>
-                                <List component="div" disablePadding>
-                                    <ListItemButton sx={{ pl: 4 }}>
-                                        <ListItemIcon>
-                                            <StarBorder />
-                                        </ListItemIcon>
-                                        <ListItemText primary="Starred" />
-                                    </ListItemButton>
+            <div className='main-grid-component'>
+                <Grid item xs={12} md={12}>
+                    <Demo>
+                        <Typography variant="h6" style={{ padding: '1rem 0rem', textDecoration: 'underline' }}>
+                            To Watch List
+                        </Typography>
+                        <ListItemButton onClick={handleClick}>
+                            <ListItemIcon>
+                                <PlaylistAddCheckRoundedIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="To Watch List" />
+                            {open ? <ExpandLess /> : <ExpandMore />}
+                        </ListItemButton>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            {movieWatchList.map(x => (
+                                <List disablePadding>
+                                    <ListItem
+                                        secondaryAction={
+                                            <IconButton edge="end" aria-label="delete" onClick={() => remove(x.id)}>
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        }
+                                    >
+                                        <ListItemButton onClick={() => redirect(x.id)}>
+                                            <ListItemAvatar>
+                                                <Avatar>
+                                                    {x.poster_path ? <Avatar src={`${base_url}${x.poster_path}`} /> : <FolderIcon />}
+                                                </Avatar>
+                                            </ListItemAvatar>
+                                            <ListItemText
+                                                primary={x.title}
+                                            />
+                                        </ListItemButton>
+                                    </ListItem>
                                 </List>
-                            </Collapse>
-                        </List>
-                    ))}
-                </Demo>
-            </Grid>
+                            ))}
+                        </Collapse>
+                    </Demo>
+                </Grid>
+            </div>
         </>
     )
 }
